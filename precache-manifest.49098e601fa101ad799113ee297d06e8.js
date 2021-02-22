b0880fd8ba0c3324ddc62ef9ad42f3c9c9914128
Nimish Bhandari <nimishbhandari727521@gmail.com>
2021-02-22 13:24:54 +0530

    Updates

---
+++ precache-manifest.49098e601fa101ad799113ee297d06e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d8753bb09783e7829c6a5b5362b249c",
+    "url": "/nimishbhandari/VDSS.git/static/media/portfolio.6d8753bb.svg"
+  },
+  {
+    "revision": "d0bc371347209ff3e315c192ffcb6d64",
+    "url": "/nimishbhandari/VDSS.git/static/media/home.d0bc3713.jpg"
+  },
+  {
+    "revision": "8afb54db6903fb586f109c98bf5fd2b0",
+    "url": "/nimishbhandari/VDSS.git/static/media/contact.8afb54db.svg"
+  },
+  {
+    "revision": "21a1d972c791a0a1c1c582bd9371719b",
+    "url": "/nimishbhandari/VDSS.git/static/media/aboutSvg.21a1d972.svg"
+  },
+  {
+    "revision": "b019227f5d70655c3fe7",
+    "url": "/nimishbhandari/VDSS.git/static/js/runtime~main.d015a57c.js"
+  },
+  {
+    "revision": "38398a1e6096bcb8d0d1",
+    "url": "/nimishbhandari/VDSS.git/static/js/main.0ca5facc.chunk.js"
+  },
+  {
+    "revision": "9693f6f832811abc937c",
+    "url": "/nimishbhandari/VDSS.git/static/js/2.7819fa93.chunk.js"
+  },
+  {
+    "revision": "38398a1e6096bcb8d0d1",
+    "url": "/nimishbhandari/VDSS.git/static/css/main.33420ceb.chunk.css"
+  },
+  {
+    "revision": "b679c0112b9d29bb5e2e4d24b45ffcde",
+    "url": "/nimishbhandari/VDSS.git/index.html"
+  }
+];
